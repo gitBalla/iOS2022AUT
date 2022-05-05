852827ecb26a1293dc72d8c36d3fd5aeb7b30be2
Nicholasdluong <nicholasdluong@gmail.com>
2022-05-05 21:18:13 +1000

    Setup MultiPeerConnectivity (MC) View controllers and initial functions

---
+++ iOSAppDev.Assign3/Controller/MCCreateViewController.swift
@@ -0,0 +1,43 @@
+//
+//  MCCreateViewController.swift
+//  iOSAppDev.Assign3
+//
+//  Created by Nicholas on 5/5/22.
+//
+
+import UIKit
+import MultipeerConnectivity
+
+class MCCreateViewController: UIViewController, MCSessionDelegate,MCBrowserViewControllerDelegate {
+    func browserViewControllerDidFinish(_ browserViewController: MCBrowserViewController) {
+        dismiss(animated: true)
+    }
+    
+    func browserViewControllerWasCancelled(_ browserViewController: MCBrowserViewController) {
+        dismiss(animated: true)
+    }
+    
+    
+    func session(_ session: MCSession, peer peerID: MCPeerID, didChange state: MCSessionState) {
+    }
+    
+    func session(_ session: MCSession, didReceive data: Data, fromPeer peerID: MCPeerID) {
+    }
+    
+    func session(_ session: MCSession, didReceive stream: InputStream, withName streamName: String, fromPeer peerID: MCPeerID) {
+    }
+    
+    func session(_ session: MCSession, didStartReceivingResourceWithName resourceName: String, fromPeer peerID: MCPeerID, with progress: Progress) {
+    }
+    
+    func session(_ session: MCSession, didFinishReceivingResourceWithName resourceName: String, fromPeer peerID: MCPeerID, at localURL: URL?, withError error: Error?) {
+    }
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+
+}
