e381e991ff017f3c10bb7eff6eb3a101e3b24994
Nicholasdluong <nicholasdluong@gmail.com>
2022-05-05 21:33:36 +1000

    Restructured MCSession extensions, created PeerID and sessions for connecting devices

--- iOSAppDev.Assign3/Controller/MCCreateViewController.swift
+++ iOSAppDev.Assign3/Controller/MCCreateViewController.swift
@@ -8,17 +8,45 @@
 import UIKit
 import MultipeerConnectivity
 
-class MCCreateViewController: UIViewController, MCSessionDelegate,MCBrowserViewControllerDelegate {
+class MCCreateViewController: UIViewController {
+
+    
+    var peerID: MCPeerID!
+    var session: MCSession!
+    var advertiserAssistant: MCAdvertiserAssistant!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        peerID = MCPeerID(displayName: UIDevice.current.name)
+        session = MCSession(peer: peerID)
+        session.delegate = self
+
+    }
+}
+
+extension MCCreateViewController: MCBrowserViewControllerDelegate {
     func browserViewControllerDidFinish(_ browserViewController: MCBrowserViewController) {
-        dismiss(animated: true)
+            dismiss(animated: true)
     }
     
     func browserViewControllerWasCancelled(_ browserViewController: MCBrowserViewController) {
-        dismiss(animated: true)
+            dismiss(animated: true)
     }
-    
+}
+
+extension MCCreateViewController: MCSessionDelegate {
     
     func session(_ session: MCSession, peer peerID: MCPeerID, didChange state: MCSessionState) {
+        switch state {
+        case .connected:
+            print("Connected: \(peerID.displayName)")
+        case .connecting:
+            print("Connecting: \(peerID.displayName)")
+        case .notConnected:
+            print("Not Connected: \(peerID.displayName)")
+        @unknown default:
+            print("Fatal error :(")
+        }
     }
     
     func session(_ session: MCSession, didReceive data: Data, fromPeer peerID: MCPeerID) {
@@ -32,12 +60,4 @@ class MCCreateViewController: UIViewController, MCSessionDelegate,MCBrowserViewC
     
     func session(_ session: MCSession, didFinishReceivingResourceWithName resourceName: String, fromPeer peerID: MCPeerID, at localURL: URL?, withError error: Error?) {
     }
-    
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-    }
-    
-
 }
