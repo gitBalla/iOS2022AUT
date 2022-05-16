705e5ee2fb6237464f0caeec8a34afc53bb6d319
johnballauts <john.a.balla@student.uts.edu.au>
2022-05-16 16:10:36 +1000

    Branch set up, new view for testing the api information display

---
+++ iOSAppDev.Assign3/Controller/TestMenuViewController.swift
@@ -0,0 +1,19 @@
+//
+//  TestMenuViewController.swift
+//  iOSAppDev.Assign3
+//
+//  Created by John Balla on 16/5/2022.
+//
+
+import GooglePlaces
+import UIKit
+
+class TestMenuViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
