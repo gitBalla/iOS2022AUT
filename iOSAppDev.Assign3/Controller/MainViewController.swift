3d8d4a75fe2f71fe1a7b90d1461f5d0762a3a815
Skarim-1 <101931176+Skarim-1@users.noreply.github.com>
2022-05-20 13:12:09 +1000

    Revert "MainViewController: Sliding Cards"
    This reverts commit 0f4ef918281466b6765610a1997259f8c3501313.

---
+++ iOSAppDev.Assign3/Controller/MainViewController.swift
@@ -0,0 +1,19 @@
+//
+//  MainViewController.swift
+//  iOSAppDev.Assign3
+//
+//  Created by John Balla on 3/5/2022.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ iOSAppDev.Assign3/Controller/MenuViewController.swift
@@ -0,0 +1,19 @@
+//
+//  MenuViewController.swift
+//  iOSAppDev.Assign3
+//
+//  Created by John Balla on 3/5/2022.
+//
+
+import GooglePlaces
+import UIKit
+
+class MenuViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
